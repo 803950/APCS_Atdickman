e6294c17350769bcfd36b021c5b44e97bffedb37
803950 <54371370+803950@users.noreply.github.com>
2019-09-22 13:48:13 -0700

    commit.sentence_builder

--- Lab920/SentenceBuilder.java
+++ Lab920/SentenceBuilder.java
@@ -1,4 +1,3 @@
-
 /**
  * builds sentences
  *
@@ -10,20 +9,43 @@ public class SentenceBuilder
 {
     String[] nouns = {"apple","carrot","pumpkin","potato","computer","pencil","pen","paper","shirt","sock","dog","cat","man","lemon","mouse","door","house","fox","book","business","case","child","company","country","day"}; //length = 25
     String[] verbs = {"jumped","ran","flew","ate","made","chopped","wanted","sang","threw","drew"}; //length = 10
-    String[] adjectives = {"litle","big","communist","ugly","slippery","warm","hopeful","grateful","hungry","serious","quick", "brown","lazy","purpe","blue"}; //lengh = 15
-    String[] determiners = {"the","my","your","his","hers","our","their","John's","Bob's","Joe's"}; //length = 10
-    String[] adverbs = {"quickly","happily","smoothly","often","very","never","always","peacefully","frightfully","like a boss"}; //length = 10
+    String[] adjectives = {"litle","big","communist","ugly","slippery","warm","hopeful","grateful","hungry","serious","quick", "brown","lazy","purple","blue"}; //lengh = 15
+    String[] determiners = {"the","my","your","his","her","our","their","John's","Bob's","Joe's"}; //length = 10
+    String[] adverbs = {"quickly","happily","smoothly","often","never","always","peacefully","frightfully","like a boss"}; //length = 10
     String[] prepositions = {"over","under","near","far from","on top of","underneath","behind","in front of","next to","inside of"}; //length = 10
-    public void loadSentences(){
-        
-    }
-    public void getSentences(){
-        
+    public String getRandom(String[] wordarray){
+        int index = (int)(Math.random()*wordarray.length);
+        return wordarray[index];
+    }    
+    
+    public String getSentences(){
+        return("default");
     }
     public String getRandomNoun(){
-        int index = (int)(Math.random()*nouns.length+1);
-        return nouns[index];
+        return(getRandom(nouns));
     }
-    System.out.print(getRandomNoun());
-}
+    public String getRandomVerb(){
+        return(getRandom(verbs));
+    }
+    public String getRandomAdjective(){
+        return(getRandom(adjectives));
+    }
+    public String getRandomAdverb(){
+        return(getRandom(adverbs));
+    }
+    public String getRandomDeterminer(){
+        return(getRandom(determiners));
+    }
+    public String getRandomPreposition(){
+        return(getRandom(prepositions));
+    }
+    public String getNounPhrase(){
+        return(getRandomDeterminer() + " " + getRandomAdjective() + " " + getRandomNoun());
+    }
+    public String getVerbPhrase(){
+        return(getRandomAdverb() + " " + getRandomVerb());
+    }
+    public String getPrepositionalPhrase(){
+       return(getRandomPreposition() + " " + getRandomDeterminer() + " " + getRandomAdjective() + " " + getRandomNoun());
+    }
 }
--- Lab920/SentenceRunner.java
+++ Lab920/SentenceRunner.java
@@ -1,12 +1,18 @@
-
-/**
- * Write a description of class SentenceRunner here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+    
+    /**
+     * Write a description of class SentenceRunner here.
+     *
+     * @author (your name)
+     * @version (a version number or a date)
+     */
 public class SentenceRunner
 {
-    SentenceRunner SentenceBuilder = new SentenceRunner();
-    
+    public static void main(){
+        SentenceBuilder sentence = new SentenceBuilder();
+        System.out.println("Sentence:     ");
+        String noun_phrase = sentence.getNounPhrase();
+        String verb_phrase = sentence.getVerbPhrase();
+        String prepositional_phrase = sentence.getPrepositionalPhrase();
+        System.out.println(noun_phrase + " " + verb_phrase + " " + prepositional_phrase);
+    }
 }
