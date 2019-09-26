cd92be32817ea311f1044bad2a6ce5285210c404
803950 <54371370+803950@users.noreply.github.com>
2019-09-26 10:25:50 -0700

    commit.lab924_Scanner_Test

--- Lab924/Person.java
+++ Lab924/Person.java
@@ -7,24 +7,59 @@
  */
 public class Person
 {
-    String fName;
-    String mName;
-    String lName;
+    private String fName;
+    private String mName;
+    private String lName;
     public Person(String fullName){
         fName = parseName(fullName)[0];
         mName = parseName(fullName)[1];
         lName = parseName(fullName)[2];
     }
-    // format 1
+    // format 1 = "Last,First Middle"
+    // format 2 = "Last,First"
+    // format 3 = "First Middle Last"
+    // format 4 = "First Last"
     public String[] parseName(String fullName){
         String[] name_array = new String[3];
-        int len_index = fullName.length();
-        int comma_index = fullName.indexOf(",");
-        name_array[2] = fullName.substring(0,comma_index);
-        int space_index = fullName.indexOf(" ");
-        name_array[0] = fullName.substring((comma_index),space_index);
-        name_array[1] = fullName.substring((space_index),len_index);
+        if(fullName.indexOf(",") != -1 & fullName.indexOf(" ") != -1){ // format1
+            int len_index = fullName.length();
+            int comma_index = fullName.indexOf(",");
+            name_array[2] = fullName.substring(0,comma_index);
+            int space_index = fullName.indexOf(" ");
+            name_array[0] = fullName.substring((comma_index) + 1,space_index);
+            name_array[1] = fullName.substring((space_index) + 1);
+        }
+        if(fullName.indexOf(" ") == -1 & fullName.indexOf(",") != -1){ // format2
+            int len_index = fullName.length();
+            int comma_index = fullName.indexOf(",");
+            name_array[2] = fullName.substring(0,comma_index);
+            name_array[1] = ""; // none given
+            name_array[0] = fullName.substring(comma_index + 1);
+        }
+        if(fullName.indexOf(" ") != -1 & fullName.indexOf(" ") != fullName.lastIndexOf(" ")){ // format3
+            int space_index1 = fullName.indexOf(" ");
+            int space_index2 = fullName.lastIndexOf(" ");
+            name_array[0] = fullName.substring(0,space_index1);
+            name_array[1] = fullName.substring(space_index1 + 1, space_index2);
+            name_array[2] = fullName.substring(space_index2 + 1);
+        }
+        if(fullName.indexOf(" ") != -1 & fullName.indexOf(" ") == fullName.lastIndexOf(" ") & fullName.indexOf(",") == -1){ //             int len_index = fullName.length();
+            
+            int space_index1 = fullName.indexOf(" ");
+            name_array[0] = fullName.substring(0,space_index1);
+            name_array[1] = "";
+            name_array[2] = fullName.substring(space_index1 + 1);
+        }
         return name_array;
     }
     
+    public String getFirstName(){
+        return fName;
+    }   
+    public String getMiddleName(){
+        return mName;
+    }
+    public String getLastName(){
+        return lName;
+    }
 }
--- Lab924/PersonRunner.java
+++ Lab924/PersonRunner.java
@@ -7,7 +7,7 @@
  */
 
 import java.util.Scanner;
-public class ScannerTest{
+public class PersonRunner{
     Scanner scanner = new Scanner(System.in);
     public static void main(){
         Scanner kb = new Scanner(System.in);
@@ -17,9 +17,14 @@ public class ScannerTest{
             System.out.println("Enter a name or type \"quit\" to quit");
             inputStr = kb.nextLine();
             Person person = new Person(inputStr);
-            System.out.println(inputStr);
+            System.out.println("Inputed name: " + inputStr);
+            System.out.println();
+            System.out.println("first name = " + person.getFirstName());
+            System.out.println("middle name = " + person.getMiddleName());
+            System.out.println("last name = " + person.getLastName());
         }
         
         
     }
+
 }
