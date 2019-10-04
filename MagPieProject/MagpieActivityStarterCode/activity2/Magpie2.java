ea12e364d0c7927bdaf7182675b4b52f95886e32
803950 <54371370+803950@users.noreply.github.com>
2019-10-04 10:28:56 -0700

    magpieactivity4.commit

--- MagPieProject/MagpieActivityStarterCode/activity2/Magpie2.java
+++ MagPieProject/MagpieActivityStarterCode/activity2/Magpie2.java
@@ -4,7 +4,7 @@
  * <ul><li>
  *       Uses indexOf to find strings
  * </li><li>
- * 		    Handles responding to simple words and phrases 
+ *          Handles responding to simple words and phrases 
  * </li></ul>
  * This version uses a nested if to handle default responses.
  * @author Laurie White
@@ -12,71 +12,152 @@
  */
 public class Magpie2
 {
-	/**
-	 * Get a default greeting 	
-	 * @return a greeting
-	 */
-	public String getGreeting()
-	{
-		return "Hello, let's talk.";
-	}
-	
-	/**
-	 * Gives a response to a user statement
-	 * 
-	 * @param statement
-	 *            the user statement
-	 * @return a response based on the rules given
-	 */
-	public String getResponse(String statement)
-	{
-		String response = "";
-		if (statement.indexOf("no") >= 0)
-		{
-			response = "Why so negative?";
-		}
-		else if (statement.indexOf("mother") >= 0
-				|| statement.indexOf("father") >= 0
-				|| statement.indexOf("sister") >= 0
-				|| statement.indexOf("brother") >= 0)
-		{
-			response = "Tell me more about your family.";
-		}
-		else
-		{
-			response = getRandomResponse();
-		}
-		return response;
-	}
+    /**
+     * Get a default greeting   
+     * @return a greeting
+     */
+    public String getGreeting()
+    {
+        return "Hello, let's talk.";
+    }
 
-	/**
-	 * Pick a default response to use if nothing else fits.
-	 * @return a non-committal string
-	 */
-	private String getRandomResponse()
-	{
-		final int NUMBER_OF_RESPONSES = 4;
-		double r = Math.random();
-		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
-		String response = "";
-		
-		if (whichResponse == 0)
-		{
-			response = "Interesting, tell me more.";
-		}
-		else if (whichResponse == 1)
-		{
-			response = "Hmmm.";
-		}
-		else if (whichResponse == 2)
-		{
-			response = "Do you really think so?";
-		}
-		else if (whichResponse == 3)
-		{
-			response = "You don't say.";
-		}
+    /**
+     * Gives a response to a user statement
+     * 
+     * @param statement
+     *            the user statement
+     * @return a response based on the rules given
+     */
+    public String getResponse(String statement)
+    {
+        String response = "";
+        statement = statement.toLowerCase();
+        if (statement.indexOf("no") >= 0)
+        {
+            response = "Why so negative?";
+        }
+        else if (statement.indexOf("mother") >= 0
+        || statement.indexOf("father") >= 0
+        || statement.indexOf("sister") >= 0
+        || statement.indexOf("brother") >= 0)
+        {
+            response = "Tell me more about your family.";
+        }
+        else if (statement.indexOf("dog") >= 0
+        || statement.indexOf("cat") >= 0)
+        {
+            response = "Tell me more about your pets.";
+        }
+        else if (statement.indexOf("mr.") >= 0
+        || statement.indexOf("ms.") >= 0){
+            response = "They sound like a good teacher.";
+        }
+        else if(statement.trim().length() == 0){
+            response = "Talk to me!";
+        }
+        else if(statement.indexOf("I want") <= 0 & statement.length()>6){
+        statement = statement.trim();
+        String lastChar = statement.substring(statement
+                .length() - 1);
+        if (lastChar.equals("."))
+        {
+            statement = statement.substring(0, statement
+                    .length() - 1);
+        }
+        int psn = findKeyword (statement, "I want", 0);
+        String restOfStatement = statement.substring(psn + 6).trim();
+        return "Would you really be happy if you had " + restOfStatement + "?";
+        }
+        else if(statement.indexOf("I") != -1 & statement.indexOf("you") > statement.indexOf("I")){
+        statement = statement.trim();
+        String lastChar = statement.substring(statement
+                .length() - 1);
+        if (lastChar.equals("."))
+        {
+            statement = statement.substring(0, statement
+                    .length() - 1);
+        }
+        int iindex = statement.indexOf(" I ") + 1;
+        int youindex = statement.indexOf(" you ") + 1;
+        String restOfStatement = statement.substring(iindex,youindex).trim();
+        return "Would you really be happy if you had " + restOfStatement + "?";
+        }
+        // Question.Activity2.1: There is a keyword 
+        //    inside a larger word that has a different meaning.
+        //    For example, the computer looks at "know" and see that substring(1,3) == "no",
+        //    so it will think the person is being negative. 
 
-		return response;
-	}
+        else
+        {
+            response = getRandomResponse();
+        }
+        return response;
+    }
+
+    /**
+     * Pick a default response to use if nothing else fits.
+     * @return a non-committal string
+     */
+    private String getRandomResponse()
+    {
+        final int NUMBER_OF_RESPONSES = 6;
+        double r = Math.random();
+        int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+        String response = "";
+
+        if (whichResponse == 0)
+        {
+            response = "Interesting, tell me more.";
+        }
+        else if (whichResponse == 1)
+        {
+            response = "Hmmm.";
+        }
+        else if (whichResponse == 2)
+        {
+            response = "Do you really think so?";
+        }
+        else if (whichResponse == 3)
+        {
+            response = "You don't say.";
+        }
+        else if (whichResponse == 4)
+        {
+            response = "I'm sorry, I don't understand.";
+        }
+        else if (whichResponse == 5)
+        {
+            response = "Could you phrase that differently?";
+        }
+        return response;
+    }
+
+    private int findKeyword(String statement, String goal, int startPos)
+    {
+        String phrase = statement.trim().toLowerCase();
+        goal = goal.toLowerCase();
+        int psn = phrase.indexOf(goal, startPos);
+        while (psn >= 0)
+        {
+            String before = " ", after = " ";
+            if (psn > 0)
+            {
+                before = phrase.substring(psn - 1, psn);
+            }
+            if (psn + goal.length() < phrase.length())
+            {
+                after = phrase.substring(psn + goal.length(),
+                    psn + goal.length() + 1);
+            }
+            /* determine the values of psn, before, and after at this point in the method. */
+            if (((before.compareTo ("a") < 0 ) || (before.compareTo("z") > 0))
+            &&
+            ((after.compareTo ("a") < 0 ) || (after.compareTo("z") > 0)))
+            {
+                return psn;
+            }
+            psn = phrase.indexOf(goal, psn + 1);
+        }
+        return -1;
+    }
 }
