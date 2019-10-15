88d4ddc05cf34a7ecf01dd27169ced98f402b5c3
Tal Dickman <tdickman@WS-ACADBUS.EDU>
2019-10-15 10:44:17 -0700

    commit.1015

---
+++ MagPieProject/MagpieActivityStarterCode/Lab1015/MMM.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class MeanMedianMode here.
+ *
+ * @author tdickman
+ * @version 1015
+ */
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class MMM {
+    public static void input() {
+    	
+    	Scanner input = new Scanner(System.in);
+    	
+    	System.out.println("Instructions:");
+    	System.out.println("Input number to add to number ArrayList");
+    	System.out.println("Type \"quit\" to finish adding numbers");
+    	while(input != "quit"){
+    	    int number = input.nextInt();
+    	    
+    	   }
+    	   }
+}
