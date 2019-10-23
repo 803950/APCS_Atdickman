e51970990a78dbbea030b83ea33262c4fb74604c
803950 <54371370+803950@users.noreply.github.com>
2019-10-22 19:38:15 -0700

    commit.lab1024_home

--- Q2/Lab1024Sorting/SortTester.java
+++ Q2/Lab1024Sorting/SortTester.java
@@ -10,13 +10,13 @@ import java.util.concurrent.TimeUnit;
 public class SortTester
 {
     static ArrayList<Integer> arrlist = new ArrayList<>();
-    private static int len = 10000;
+    private static int len = 100; // defines length of random array
     private static int swapcounter = 0;
     private static int compcounter = 0;
     private static long timeElapsed;
     public static void loadArrl(int len){
         for (int j = 0;j < len;j++) {
-            arrlist.add((int)(Math.random() * 100) + 1);
+            arrlist.add((int)(Math.random() * 1000) + 1);
         }
     }
 
@@ -24,24 +24,39 @@ public class SortTester
         // bubble sort
         loadArrl(len);
         bubbleSort();
-        System.out.print(arrlist);
+        System.out.print("(Bubble Sort) ");
+        printList();
         arrlist.clear();
         System.out.println();
         System.out.println("Number of comparisons: " + compcounter);
         System.out.println("Number of swaps: " + swapcounter);
-        System.out.println("Duration in nanoseconds :" + timeElapsed);
+        System.out.println("Duration :" + (double)timeElapsed/1000000 + " millisecond");
 
         // insert sort
+        System.out.println();
         loadArrl(len);
         insertionSort();
-        System.out.print(arrlist);
+        System.out.print("(Insertion Sort) ");
+        printList();
         arrlist.clear();
         System.out.println();
         System.out.println("Number of comparisons: " + compcounter);
         System.out.println("Number of swaps: " + swapcounter);
-        System.out.println("Duration in nanoseconds :" + timeElapsed);
+        System.out.println("Duration :" + (double)timeElapsed/1000000 + " milliseconds");
+        
+        // selection sort
+        System.out.println();
+        loadArrl(len);
+        selectSort();
+        System.out.print("(Selection Sort) ");
+        printList();
+        arrlist.clear();
+        System.out.println();
+        System.out.println("Number of comparisons: " + compcounter);
+        System.out.println("Number of swaps: " + swapcounter);
+        System.out.println("Duration :" + (double)timeElapsed/1000000 + " milliseconds");
+        
     }
-
     public static void bubbleSort(){
         timeElapsed = 0;
         swapcounter = 0;
@@ -87,6 +102,41 @@ public class SortTester
         long endTime = System.nanoTime();
         timeElapsed = endTime - startTime;
     }
+
+    public static void selectSort(){
+        timeElapsed = 0;
+        swapcounter = 0;
+        compcounter = 0;
+        long startTime = System.nanoTime();
+        int temp = 0;
+        for (int i = 0; i < arrlist.size() - 1; i++)
+        {
+            int index = i;
+            for(int j = i+1; j<arrlist.size(); j++){
+                if (arrlist.get(j) < arrlist.get(index)){
+                    swap(arrlist,index,j);
+                    compcounter++;
+                    swapcounter++;
+                }else{
+                    compcounter++;
+                }
+            }
+        }
+
+        long endTime = System.nanoTime();
+        timeElapsed = endTime - startTime;
+    }
+    
+    public static void printList(){
+        System.out.print("[");
+        for(int i = 0; i<len-1; i++){
+            System.out.print(arrlist.get(i));
+            System.out.print(", ");
+        }
+        System.out.print(arrlist.get(len-1));
+        System.out.print("]");
+    }
+    
     public static void swap(ArrayList<Integer> list, int i, int j){
         int hole = list.get(i);
         list.set(i,list.get(j));
