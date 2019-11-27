1e3c4c19ba831cfa9bb8023a2fe7f22f27301e0f
803950 <54371370+803950@users.noreply.github.com>
2019-11-26 20:39:58 -0800

    commit.mandlbrot2

--- Mandelbrot/Display.java
+++ Mandelbrot/Display.java
@@ -16,21 +16,70 @@ public class Display extends JComponent{
         frame.getContentPane().setBackground(Color.WHITE);
         frame.add(new Display());
     }
-    
+
     public void paint(Graphics g){ // this is where the pixels will be plotted
+        Pixel p = new Pixel(g);
+        int w = 1000;
+        int h = 1000;
         g.setColor(Color.BLACK);
-        int xi = 400;
-        int yi = 400;
-        int size = 10;
-        for(int i = 0; i < 100/size; i++){ // testing code, fills a 100*100 square with pixels
-            for(int j = 0; j< 100/size; j++){
-                Pixel.newPixel(g,(xi + size*j*2),(yi + size*i*2),size);
+        int size = 1;
+        int iterations = 1000;
+        for(int i = 0; i < w/size; i++){
+            for(int j = 0; j< h/size; j++){
+                p.setX(j);
+                p.setY(i);
+                int color = calculateColor(j,i,iterations,4.0);
+                if(color == 1){ // if magnitude is less than the max
+                    p.newPixel(g,j,i,size); // draw a pixel
+                }
             }
         }
     }
-    
+
     public Display(){
-        
+
     }
-    
+
+    public static int iterator(Pixel p, int max){
+        int marker = 0;
+        double a = (double)p.getX();
+        double b = (double)p.getX();
+        Complex zf;
+        Complex zi = new Complex(0,0); // z0 is the z at iteration 0.
+        Complex complix = new Complex(a,b);
+        int iterations = 100;
+        zf = iterate(complix,zi,iterations);
+        if(zf.abs() > max){
+            marker = 1;
+        }
+        return marker;
+    }
+
+    public static Complex iterate(Complex constant, Complex zi, int iterations){
+        if(iterations == 1){
+            return zi;
+        }else{
+            Complex zf = zi.times(zi).plus(constant);
+            iterate(constant,zf,iterations-1);
+            return zf;
+        }
+    }
+
+    public int calculateColor(double x, double y,int iterations, double max){
+        int marker = 1;
+        double cx = x;
+        double cy = y;
+        for(int i = 0; i<iterations; i++){
+            double nx = x*x - y*y + cx;
+            double ny = 2*x*y + cy;
+            x = nx;
+            y = ny;
+            if(x*x + y*y > max){ // if it "diverges" (passes the maximum)
+                break;
+            }else{
+                marker = 0;
+            }
+        }
+        return marker;
+    }
 }
--- Mandelbrot/Pixel.java
+++ Mandelbrot/Pixel.java
@@ -6,12 +6,39 @@ import javax.swing.JComponent;
 import java.awt.*;
 public class Pixel
 {
-    public Pixel(Graphics f, int x, int y, int size){
+    private double x;
+    private double y;
+    private double size;
+    public Pixel(Graphics f){
         newPixel(f,x,y,size);
     }
     
-    public static void newPixel(Graphics f, int x,int y,int size){
+    public static void newPixel(Graphics f, double x,double y,double size){
         f.setColor(Color.BLACK);
-        f.fillRect(x,y,size,size);
+        f.fillRect((int)x,(int)y,(int)size,(int)size);
+    }
+    
+    public double getX(){
+        return x;
+    }
+    
+    public double getY(){
+        return y;
+    }
+    
+    public double getSize(){
+        return size;
+    }
+    
+    public void setX(double a){
+        this.x = a;
+    }
+    
+    public void setY(double a){
+        this.y = a;
+    }
+    
+    public void setSize(double a){
+        this.size = a;
     }
 }
