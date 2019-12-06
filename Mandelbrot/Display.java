b01a7e3c5a60675986ae5add5ef2069f4f6052dc
803950 <54371370+803950@users.noreply.github.com>
2019-12-05 21:48:01 -0800

    commit.1205home

--- Mandelbrot/Display.java
+++ Mandelbrot/Display.java
@@ -2,25 +2,27 @@ import javax.imageio.ImageIO;
 import java.io.*;
 import java.awt.image.*;
 public class Display{
-    public static void main(String[] args) throws Exception{
-        int h = 8000;
-        int w = 8000;
+    private static int h = 10000;
+    private static int w = 10000;
+    public static void main() throws Exception{
         int iterations = 100;
         Pixel[][] picture = new Pixel[h][w];
+        double convergence = 0;
         for(int i = 0; i<h; i++){
             for(int j = 0; j<w; j++){
-                if(iterate((float)(i-w/2f)/(h/3),(float)(j-h/2f)/(w/3),iterations) == 0){
+                convergence = iterate((float)(i-w/2f)/(h/3),(float)(j-h/2f)/(w/3),iterations);
+                if(convergence == 0.0){ // determines black regions
                     picture[i][j] = new Pixel(0,0,0);
                 }else{
-                    picture[i][j] = new Pixel(255,255,255);
+                    picture[i][j] = new Pixel((int)(255*convergence),255,(int)(210*convergence)); // choose colors here
                 }
             }
         }
         outputPicture(picture);
     }
 
-    public static int iterate(float a, float b, int iterations){
-        int convergence = 0;
+    public static double iterate(float a, float b, int iterations){
+        double convergence = 0.0;
         float cx = a;
         float cy = b;
         int i = 0;
@@ -30,28 +32,25 @@ public class Display{
             a = nx;
             b = ny;
             if(a*a + b*b > 4){
+                convergence = 4.0/(a*a + b*b);
                 break;
             }
         }
         if(i == iterations){
-            convergence = 0;
-        }else{
-            convergence = 1;
+            convergence = 0.0;
         }
         return convergence;
     }
 
     public static void outputPicture(Pixel[][] picture) throws Exception{
-        int l = picture.length;
-        int w = picture[0].length;
-        int[] pixels = new int[l*w];
-        for(int i = 0; i<l; i++){
+        int[] pixels = new int[h*w];
+        for(int i = 0; i<h; i++){
             for(int j = 0; j<w; j++){
                 pixels[i*w + j] = picture[i][j].getColorInt();
             }
         }
-        BufferedImage image = new BufferedImage(w,l,BufferedImage.TYPE_INT_RGB);
-        image.setRGB(0,0,w,l,pixels,0,w);
+        BufferedImage image = new BufferedImage(w,h,BufferedImage.TYPE_INT_RGB);
+        image.setRGB(0,0,w,h,pixels,0,w);
         ImageIO.write(image, "bmp", new File("output.bmp") );
         System.out.println("image created");   
     }
