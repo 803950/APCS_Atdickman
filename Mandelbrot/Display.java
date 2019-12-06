a29dbabb1099c2c510cd4bd86dd7eeb2703a6c79
803950 <54371370+803950@users.noreply.github.com>
2019-12-05 22:08:26 -0800

    commit.1205home

--- Mandelbrot/Display.java
+++ Mandelbrot/Display.java
@@ -4,13 +4,13 @@ import java.awt.image.*;
 public class Display{
     private static int h = 10000;
     private static int w = 10000;
-    public static void main() throws Exception{
+    public static void mandelbrot() throws Exception{
         int iterations = 100;
         Pixel[][] picture = new Pixel[h][w];
         double convergence = 0;
         for(int i = 0; i<h; i++){
             for(int j = 0; j<w; j++){
-                convergence = iterate((float)(i-w/2f)/(h/3),(float)(j-h/2f)/(w/3),iterations);
+                convergence = iterateM((float)(i-w/2f)/(h/3),(float)(j-h/2f)/(w/3),iterations);
                 if(convergence == 0.0){ // determines black regions
                     picture[i][j] = new Pixel(0,0,0);
                 }else{
@@ -21,7 +21,23 @@ public class Display{
         outputPicture(picture);
     }
 
-    public static double iterate(float a, float b, int iterations){
+        public static void julia() throws Exception{
+        int iterations = 10;
+        Pixel[][] picture = new Pixel[h][w];
+        double convergence = 0;
+        for(int i = 0; i<h; i++){
+            for(int j = 0; j<w; j++){
+                convergence = iterateJ((float)(i-w/2f)/(h/3),(float)(j-h/2f)/(w/3),iterations);
+                if(convergence == 0.0){ // determines black regions
+                    picture[i][j] = new Pixel(0,0,0);
+                }else{
+                    picture[i][j] = new Pixel((int)(255*convergence),255,(int)(210*convergence)); // choose colors here
+                }
+            }
+        }
+        outputPicture(picture);
+    }
+    public static double iterateM(float a, float b, int iterations){
         double convergence = 0.0;
         float cx = a;
         float cy = b;
@@ -29,6 +45,29 @@ public class Display{
         for(;i<iterations;i++){
             float nx = a*a - b*b + cx;
             float ny = 2*a*b + cy;
+            a = nx;
+            b = ny;
+            if(a*a + b*b > 4){
+                convergence = 4.0/(a*a + b*b);
+                break;
+            }
+        }
+        if(i == iterations){
+            convergence = 0.0;
+        }
+        return convergence;
+    }
+
+    
+ 
+    public static double iterateJ(float a, float b, int iterations){
+        double convergence = 0.0;
+        float cx = a;
+        float cy = b;
+        int i = 0;
+        for(;i<iterations;i++){
+            float nx = a*a*a*a*a*a*a - 21*a*a*a*a*a*b*b + 35*a*a*a*b*b*b*b - 7*a*b*b*b*b*b*b + cx;
+            float ny = -1*b*b*b*b*b*b*b + 21*a*a*b*b*b*b*b - 35*a*a*a*a*b*b*b +7*a*a*a*a*a*a*b + cy;
             a = nx;
             b = ny;
             if(a*a + b*b > 4){
