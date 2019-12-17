a97a0e1bdaf34efb05a9f439342e9ee3f8858242
Tal Dickman <tdickman@WS-ACADBUS.EDU>
2019-12-17 10:31:18 -0800

    commit.12172

--- Mandelbrot/Display.java
+++ Mandelbrot/Display.java
@@ -4,8 +4,8 @@ import java.awt.image.*;
 import java.util.*;
 import java.lang.Math;
 public class Display{
-    static final int h = 1600; // make upper case
-    static int w = 400;
+    static final int h = 4800; // make upper case
+    static int w = 1200;
     static int iterations = 100;
 
     public static void main(String[] args) throws Exception{
